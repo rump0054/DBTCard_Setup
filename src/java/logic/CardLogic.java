90f75205a9cc6ac947c120a8fa1b87b045ed5134
Erik Rumppe <erumppe@gmail.com>
2012-03-05 15:53:34 -0800

    Before Refactoring of day.ok.vm page layout

--- src/java/logic/CardLogic.java
+++ src/java/logic/CardLogic.java
@@ -15,177 +15,165 @@ import org.vraptor.annotations.Out;
 import org.vraptor.annotations.Parameter;
 
 @Component("card")
-public class CardLogic
-{    
+public class CardLogic {
+
     @In
     private HttpServletRequest request;
-    
     @In
     private HttpSession session;
-    
     @Parameter
     private String datekey;
-    
     @Parameter
     private long targetID;
-    
-    @Parameter(create=true)
+    @Parameter(create = true)
     private ArrayList<Target> targs;
-    
-    @Parameter(create=true)
+    @Parameter(create = true)
     private Card dayCard;
- 
     @Out
     private String keydate;
-    
     @Out
     private Card card;
-    
     @Out
     private String msgs;
-    
-    // Empty method that sends you to the month calendar page
-    public void month() {}
-    
-    // Method determines if forward to card setup or day page
-    public String day()
-    {
-        String username = "erumppe";  //request.getRemoteUser();
-        
+
+    // Method determines if user goes to day or setup pages
+    public String direct() {
+        String username = "erumppe";
+        keydate = datekey;
+
+        // Remove card from session in case it was left by setup
+        session.removeAttribute("card");
+
         boolean needCard = CardDAO.needCard(username, datekey);
-        if(!needCard)
-        {
-            card = new Card();
-            card = CardDAO.getCard(username, datekey);
-            card.setDatekey(datekey);
-            
-            return "ok";
-        }
-        else
-        {
-            if(session.getAttribute("card") != null && session.getAttribute("card") != "")
-            {
-                card = (Card)session.getAttribute("card");
-                card.setDatekey(datekey);
-            }
-            else
-            {
-                card = new Card();
-                card.setUsername(username);
-                card.setWeekStart(datekey);
-                card.setDatekey(datekey);
-            
-                card = CardDAO.setupCard(card);
-                
-                session.setAttribute("card", card);
-            }
-            
+        if (!needCard) {
+            return "day";
+        } else {
             return "setup";
         }
     }
- 
-    // Disable a target from the card setup screen
-    public void disableTarget()
-    {
+
+    // Empty method that sends you to the month calendar page
+    public void month() {
+    }
+
+    // Method determines if forward to card setup or day page
+    public String day() {
+        String username = "erumppe";  //request.getRemoteUser();
+
         card = new Card();
-        card = (Card)session.getAttribute("card");
+        card = CardDAO.getCard(username, datekey);
+        card.setDatekey(datekey);
+
+        return "ok";
+    }
+
+    public void setup()
+    {
+        String username = "erumppe";
         
-        ArrayList<Target> targets = new ArrayList<Target>();
-        for(Target t : card.getTargets())
+        if(session.getAttribute("card") != null &&
+           session.getAttribute("card") != "")
         {
-            if(t.getTargetID() != targetID)
-            {
+            card = (Card)session.getAttribute("card");
+            card.setDatekey(datekey);
+        }
+        else
+        {
+            card = new Card();
+            card.setUsername(username);
+            card.setWeekStart(datekey);
+            card.setDatekey(datekey);
+            
+            card = CardDAO.setupCard(card);
+            
+            session.setAttribute("card", card);
+        }
+    }
+
+    // Disable a target from the card setup screen
+    public void disableTarget() {
+        card = new Card();
+        card = (Card) session.getAttribute("card");
+
+        ArrayList<Target> targets = new ArrayList<Target>();
+        for (Target t : card.getTargets()) {
+            if (t.getTargetID() != targetID) {
                 targets.add(t);
-            }
-            else
-            {
+            } else {
                 t.setActive(false);
                 targets.add(t);
             }
         }
-        
+
         Collections.sort(targets);
-        
+
         card.setTargets(targets);
-        session.setAttribute("card", card); 
+        session.setAttribute("card", card);
     }
 
     // Enable a target from the card setup screen
-    public void enableTarget()            
-    {
+    public void enableTarget() {
         card = new Card();
-        card = (Card)session.getAttribute("card");
-        
+        card = (Card) session.getAttribute("card");
+
         ArrayList<Target> targets = new ArrayList<Target>();
-        for(Target t : card.getTargets())
-        {
-            if(t.getTargetID() != targetID)
-            {
+        for (Target t : card.getTargets()) {
+            if (t.getTargetID() != targetID) {
                 targets.add(t);
-            }
-            else
-            {
+            } else {
                 t.setActive(true);
                 targets.add(t);
             }
         }
-        
+
         Collections.sort(targets);
-        
+
         card.setTargets(targets);
-        session.setAttribute("card", card);        
+        session.setAttribute("card", card);
     }
-    
+
     // Add a target to the database and the card setup screen
-    public void addTarget(Target target)
-    {
+    public void addTarget(Target target) {
         card = new Card();
-        card = (Card)session.getAttribute("card");
+        card = (Card) session.getAttribute("card");
         ArrayList<Target> targets = card.getTargets();
-        
-        if(target != null && !target.getTarget().equals("") && !target.getCategory().equals(""))
-        {
+
+        if (target != null && !target.getTarget().equals("") && !target.getCategory().equals("")) {
             target.setUsername(card.getUsername());
             target = CardDAO.insertTarget(target);
             target.setActive(true);
-            
+
             targets.add(target);
-        }
-        else
-        {
+        } else {
             msgs = "An error occurred.  Please try again and make sure you enter "
-                  + "information in all the fields.";
+                    + "information in all the fields.";
         }
-        
+
         Collections.sort(targets);
-        
+
         card.setTargets(targets);
 
-        session.setAttribute("card", card); 
+        session.setAttribute("card", card);
     }
-    
+
     // Add card to the database and goto day view for datekey
-    public void addCard()
-    {
+    public void addCard() {
         card = new Card();
-        card = (Card)session.getAttribute("card");
-        
+        card = (Card) session.getAttribute("card");
+
         CardDAO.insertCard(card);
 
         // Remember to add to DB only ACTIVE targets
         session.removeAttribute("card");
     }
-    
-    public void addEntry()
-    {
+
+    public void addEntry() {
         keydate = dayCard.getDatekey();
-        
+
         CardDAO.deleteDayValues(dayCard.getCardID(), dayCard.getDaykey());
-        
-        for(Target t : targs)
-        {
-            if(t.getValue() != null && !t.getValue().equals(""))
-            {
+
+        for (Target t : targs) {
+            if (t.getValue() != null && !t.getValue().equals("")) {
                 CardDAO.insertDayValues(t);
             }
         }
